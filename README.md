eff3e9561223d4a1a6e5be4d922f97ceb7031443
Amós Rivera <arivera.utpmp@gmail.com>
2013-11-09 02:45:41 -0600

    Saving post user to text file for tests.

---
+++ README.md
@@ -0,0 +1,1 @@
+https://confluence.atlassian.com/display/BITBUCKET/POST+hook+management
--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 class Hook < Sinatra::Base
+	require 'json'
+
 	post '/' do
+		commit = JSON.parse(request.body.read)
+		
+		# save to log
+		log = File.open("/sites/git_hook/commits.log","a")
+		log.write commit['user']
+		log.write "\n"
+		log.close
+
+		200
 	end
 end
